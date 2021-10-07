d3be167b7bac4b7b1092623b8924d69dffd930f3
leejiseong <leejiseong@DESKTOP-AKI7KFF.mshome.net>
2021-10-07 15:41:49 +0900

    약결합

--- ex00/src/main/java/org/zerock/sample/Chef.java
+++ ex00/src/main/java/org/zerock/sample/Chef.java
@@ -2,6 +2,7 @@ package org.zerock.sample;
 
 import org.springframework.stereotype.Component;
 
+
 import lombok.Data;
 
 @Component
--- ex00/src/main/java/org/zerock/sample/Restaurant.java
+++ ex00/src/main/java/org/zerock/sample/Restaurant.java
@@ -1,5 +1,16 @@
 package org.zerock.sample;
 
-public class Restaurant {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Setter;
+
+@Component //이 클래스는 스프링에서 관리해야하는 대상임
+@Data
+public class Restaurant {
+	
+	@Setter(onMethod_=@Autowired)
+	private Chef chef;
 }
